a3da52db206a4eefb243b387875db0103fbcbff9
Firrow <firrowmb@gmail.com>
2020-10-21 12:28:50 +0200

    Init Manon
    Header et footer

---
+++ tete/base.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Base Site</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+	<header>
+		<div class="line">
+			<div id="blocGauche" class="line" media="(orientation:portrait)">
+			<img id="menuBurger" src="logos/Menu1.png">
+			<nav class="barreRecherche"> 
+					<form class="formBarreRecherche" role="search"> 
+						<div class="input-group"> 
+							<input type="search" class="form-control" placeholder="Search" /> 
+								<span class="input-group-btn"> 
+									<button type="button" class="boutonRecherche">	
+										<span class="glyphicon glyphicon glyphicon-search" aria-hidden="true"></span>
+									</button> 
+								</span> 
+						</div> 
+					</form> 
+				</nav>
+			</div>
+
+			<div id="void"></div>
+
+			<div id="blocDroite" class="line">
+				<img id="boutonDossier" src="logos/Fold1.png">
+				<img id="boutonDownload" src="logos/Download1.png">
+				<img id="boutonPDF" src="logos/PDF1.png"> 
+				<img id="boutonOptions" src="logos/Setting1.png">
+				<img id="boutonLogoCBS" src="logos/logoCBS.png">
+			</div>
+		</div>
+	</header>
+	
+
+<!-- 		<div id="menu_barre">
+			<div class="boutons">
+				<img id="menuBurger" src="logos/Menu1.png">
+				<img id="boutonDossier" src="logos/Fold1.png">
+				<img id="boutonDownload" src="logos/Download1.png">
+				<img id="boutonPDF" src="logos/PDF1.png"> 
+				<img id="boutonOptions" src="logos/Setting1.png">
+				<img id="boutonLogoCBS" src="logos/logoCBS.png">
+			</div>
+			
+			<nav class="barreRecherche"> 
+				<form class="formBarreRecherche" role="search"> 
+					<div class="input-group"> 
+						<input type="text" class="form-control" placeholder="Search" /> 
+							<span class="input-group-btn"> 
+								<button type="button" class="boutonRecherche">	
+									<span class="glyphicon glyphicon glyphicon-search" aria-hidden="true"></span>
+								</button> 
+							</span> 
+					</div> 
+				</form> 
+			</nav>
+		</div> -->
+
+</body>
+</html>
