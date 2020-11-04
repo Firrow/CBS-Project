80362affb9dc3a4841135ec21824cbdae54bb61f
Firrow <73218766+Firrow@users.noreply.github.com>
2020-11-04 16:32:56 +0100

    Update base.php

--- tete/base.php
+++ tete/base.php
@@ -28,28 +28,3 @@
 			</div>
 		</div>
 	</header>
-	
-
-<!-- 		<div id="menu_barre">
-			<div class="boutons">
-				<img id="menuBurger" src="logos/Menu1.png">
-				<img id="boutonDossier" src="logos/Fold1.png">
-				<img id="boutonDownload" src="logos/Download1.png">
-				<img id="boutonPDF" src="logos/PDF1.png"> 
-				<img id="boutonOptions" src="logos/Setting1.png">
-				<img id="boutonLogoCBS" src="logos/logoCBS.png">
-			</div>
-			
-			<nav class="barreRecherche"> 
-				<form class="formBarreRecherche" role="search"> 
-					<div class="input-group"> 
-						<input type="text" class="form-control" placeholder="Search" /> 
-							<span class="input-group-btn"> 
-								<button type="button" class="boutonRecherche">	
-									<span class="glyphicon glyphicon glyphicon-search" aria-hidden="true"></span>
-								</button> 
-							</span> 
-					</div> 
-				</form> 
-			</nav>
-		</div> -->
